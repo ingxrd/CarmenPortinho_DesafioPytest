fd3ccdc53a0eb16e9efc9e252085d1b297cbe086
Ingrid <152234829+ingxrd@users.noreply.github.com>
2024-10-10 15:49:05 -0300

    Teste Avançado - Ingrid

---
+++ advanced/test_advanced.py
@@ -0,0 +1,18 @@
+import pytest
+import os
+
+# Função str_to_bool
+def str_to_bool(string):
+    if string.lower() in ['yes', 'y', '1']:
+        return True
+    elif string.lower() in ['no', 'n', '0']:
+        return False
+
+# Testes para a função str_to_bool
+@pytest.mark.parametrize("string", ['Y', 'y', '1', 'YES'])
+def test_str_to_bool_true(string):
+    assert str_to_bool(string) is True
+
+@pytest.mark.parametrize("string", ['N', 'n', '0', 'NO'])
+def test_str_to_bool_false(string):
+    assert str_to_bool(string) is False
