fc9f9628d50c4fe23ed3225e5219221274253937
liviatech <li.livia.tech@gmail.com>
2024-10-11 20:57:48 -0300

    função str_to_bool() recebe uma cadeia de caracteres como entrada e seu trabalho é retornar True ou False

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -1,1 +1,11 @@
-#exercicio1
+#função str_to_bool
+def str_to_bool(value):
+    true_values = ['y','yes']
+    false_values = ['no', 'n']
+
+    if value in true_values:
+        return True
+    if value in false_values:
+        return False
+    
+    
