d34d7e9ed94de19d0545f3214ad0b1a2436a9e67
Elizabete Fabri <elizabete958@gmail.com>
2024-10-12 12:28:50 -0300

    Método final para limpar, o arquivo temporário apos a conclusão dos testes

--- advanced/test_advanced.py
+++ advanced/test_advanced.py
@@ -16,3 +16,33 @@ def test_str_to_bool_true(string):
 @pytest.mark.parametrize("string", ['N', 'n', '0', 'NO'])
 def test_str_to_bool_false(string):
     assert str_to_bool(string) is False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Método final para limpar, o arquivo temporário apos a conclusão dos testes
+@pytest.fixture
+def tmpfile(tmpdir):
+    def write():
+        file = tmpdir.join("done")
+        file.write("1")
+        return file.strpath
+    return write
