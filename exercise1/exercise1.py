cae9eda7eb09f1f578bb0a18a92cc072547283b3
Thais D'Avilla <thaisdavilla@yahoo.com.br>
2024-10-12 18:28:18 -0300

    Enviando teste exercise1.py

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -26,3 +26,21 @@ class TestStrToBool(unittest.TestCase): #a classe para testar o metodo acima
 
 if __name__ == '__main__':
     unittest.main() #executar o arquivo  diretamente
+    
+'''#correção
+def str_to_bool(value):
+    try:
+        value = value.lower()
+    except AttributeError:
+        raise AttributeError(f"{value} must be of type string")
+    true_values = ['y','yes']
+    false_values = ['no', 'n']
+
+    if value in true_values:
+        return True
+    if value in false_values:
+        return False
+    
+def test_invalid_input(self):
+        with self.assertRaises(AttributeError):
+            str_to_bool(1)'''
